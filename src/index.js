1d3e03769aba2345710f5d38bc4766a1f34d3aa6
Gqq475 <542764124@qq.com>
2016-08-11 10:45:20 +0800

    avc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
-
+import './style/mk.css';
 import './style/common.css';
 import './style/hljs.css';
-import {Router,hashHistory} from 'react-router';
+import {Router,browserHistory} from 'react-router';
 import routes from './routes.js';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
@@ -11,5 +11,5 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-render(<Router routes={routes} history={hashHistory} />
+render(<Router routes={routes} history={browserHistory} />
   ,document.getElementById('root'));
